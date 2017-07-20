feda46abfa5f3641429d2bd050b6ea56c899453b
Srikanth Mulugu <30309108+srikmulu2@users.noreply.github.com>
2017-07-20 18:19:13 +0530

    initial website commit

---
+++ public_html/default.php
@@ -0,0 +1,191 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+<head>
+<title>.:: SHRI HEMA DURGA TEMPLE ::.</title>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+  <meta name="Author" content="Srikanth Mulugu">
+  <meta name="Keywords" content="hemadurga, chandi, durga, lakshmi, saraswathi, ucchistha, ganapathi, ganesh, vinayaka, gayathri, shiva, siva, sahasralinga, linga, panchayatana, navagraha, hanuman, anjaneya, subrahamaya, murga, ganga, alwal, suryanagar, nagendara">
+  <meta name="Description" content="">
+
+<link href="images/style.css" rel="stylesheet" type="text/css">
+<script type="text/javascript">
+//<!--
+function MM_swapImgRestore() { //v3.0
+  var i,x,a=document.MM_sr; for(i=0;a&&i<a.length&&(x=a[i])&&x.oSrc;i++) x.src=x.oSrc;
+}
+function MM_preloadImages() { //v3.0
+  var d=document; if(d.images){ if(!d.MM_p) d.MM_p=new Array();
+    var i,j=d.MM_p.length,a=MM_preloadImages.arguments; for(i=0; i<a.length; i++)
+    if (a[i].indexOf("#")!=0){ d.MM_p[j]=new Image; d.MM_p[j++].src=a[i];}}
+}
+
+function MM_findObj(n, d) { //v4.01
+  var p,i,x;  if(!d) d=document; if((p=n.indexOf("?"))>0&&parent.frames.length) {
+    d=parent.frames[n.substring(p+1)].document; n=n.substring(0,p);}
+  if(!(x=d[n])&&d.all) x=d.all[n]; for (i=0;!x&&i<d.forms.length;i++) x=d.forms[i][n];
+  for(i=0;!x&&d.layers&&i<d.layers.length;i++) x=MM_findObj(n,d.layers[i].document);
+  if(!x && d.getElementById) x=d.getElementById(n); return x;
+}
+
+function MM_swapImage() { //v3.0
+  var i,j=0,x,a=MM_swapImage.arguments; document.MM_sr=new Array; for(i=0;i<(a.length-2);i+=3)
+   if ((x=MM_findObj(a[i]))!=null){document.MM_sr[j++]=x; if(!x.oSrc) x.oSrc=x.src; x.src=a[i+2];}
+}
+//-->
+</script>
+
+<!-- Google Analytics Code -->
+
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-32338015-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
+
+</HEAD>
+ <BODY onLoad="MM_preloadImages('images/gal_tag.PNG')">
+<table width="100%" border="2" cellspacing="0" cellpadding="0" class="maintable">
+  <tr> 
+    <td align="center" valign="top"> <A NAME="BK"></A>
+	
+	<table width="992" border="0" cellspacing="0" cellpadding="0">
+        <tr> 
+          <td valign="top">
+
+<!-- Includes the Header page -->
+<?php include("header.html"); ?>
+
+
+			</td>
+        </tr>
+        <tr> 
+          <td height="58" align="left" valign="top"><table width="100%" border="0" cellspacing="0" cellpadding="0">
+              <tr>
+                <td valign="top" align="left" width="200px"><a href="event.html"><img src="images/upcoming-Event.png" width="192" height="34" border="0"></a>           </td>
+    <td valign="bottom" align="left"><table width="585px" border="0" cellspacing="0" cellpadding="0">
+                          <tr> 
+                            <td height="34" align="center" valign="middle" class="menu" style="color: #fff">
+                         
+<!-- Includes the Header page -->
+<?php include("menu.html"); ?>
+							
+						   </td>
+						   </tr>
+                </table></td>
+                <td align="center" valign="top"><a href="gallery.html" onMouseOut="MM_swapImgRestore()" onMouseOver="MM_swapImage('Image9','','images/gal_tag.PNG',1)"><img src="images/gal_tag1.png" name="Image9" width="192" height="34" border="0"></a></td>
+              </tr>
+              <tr> 
+                <td width="194px" valign="top" align="left">
+				
+							
+<!-- Includes the Upcomming Events Scroll -->
+<?php include("ucomming_events.html"); ?>
+
+
+				</td>
+                <td width="585" valign="top" align="left"><table width="585" border="0" cellspacing="0" cellpadding="0">
+                    <tr> 
+                      <td height="17" bgcolor="#FFFFFF"><!--<img src="images/top.PNG">--></td>
+                    </tr>
+                    <tr> 
+                      <td align="center" class="middle"><table width="555" border="0" cellspacing="0" cellpadding="0">
+                          <tr> 
+                            <td height="30" class="tag" style="padding-left: 15px; font-size: 16px"><strong>ABOUT 
+                              <font color="#ff6600">HEMADURGA TEMPLE</font></strong></td>
+                          </tr>
+                          <tr> 
+                            <td style="padding: 10px; line-height: 20px"><div align="justify">                                
+								In 1979 with the divine grace of supreme being (goddess durga) we have started performing <a href="#SN">Durga(Sharada) Navarathri</a> pooja as  prescribed in the vedic tradition at our residence . Due to immense  grace of holy mother we are performing the Sharada Navarathri Poojas every year uninterrupted till date.<br>
+                                <br>
+								In 1999 after successful completion of 20 years of Sharada Navarathri pooja the seed to construct a temple was sown in our hearts by the goddess and we started to acquire required resources to construct the temple. Finally in 2009 things started to fall into place and we have started the construction of the temple.<br>
+                                <br>
+								We have first installed Ganga Dev and Abhaya Anjaneya swami, then Goddess Lakshmi & Ganesh. We have also planted Raavi & Vepa (Peepal & Neem) tree<br><center>
+								<img src="images/home/ganga.jpg" WIDTH="80" HEIGHT="120" BORDER="0" ALT="Ganga">
+								<IMG SRC="images/home/hanuman.jpg" WIDTH="80" HEIGHT="120" BORDER="0" ALT="Hanuman">
+								<IMG SRC="images/home/lakshmiganapathi.jpg" WIDTH="160" HEIGHT="120" BORDER="0" ALT="Lakshmi Ganapathi">
+								<IMG SRC="images/home/chandi.jpg" WIDTH="180" HEIGHT="120" BORDER="0" ALT="Shakti Panchayatanam">
+								</center>
+										<br>		
+								<IMG SRC="images/home/durga.jpg" WIDTH="180" HEIGHT="120" BORDER="0" ALT="Shakti Panchayatanam" align="right">
+								Then we have converted the temple into Shakti Panchayatana integrated with Chandi. Shakti Panchayatana has Goddess Durga in the center surrounded by Ganesh, Siva, Vishnu & Surya. Chandi is a combination of Goddess Durga, Lakshmi & Saraswathi.
+								<br><br>
+								<IMG SRC="images/home/navagrahas.jpg" WIDTH="180" HEIGHT="120" BORDER="0" ALT="Navagrahas" align="left">
+								<IMG SRC="images/home/nagendra.jpg" WIDTH="65" HEIGHT="120" BORDER="0" ALT="Nagendra" align="left"><br>
+								In 2010 for first anniversary we have installed Navagrahas and Santana Nagendara under Raavi & Vepa (Peepal & Neem)tree. 
+								<br><br>
+								<IMG SRC="images/home/srichakra.PNG" WIDTH="120" HEIGHT="120" BORDER="0" ALT="SriChakra Meru" align="right">						
+								<IMG SRC="images/home/siva.jpg" WIDTH="120" HEIGHT="120" BORDER="0" ALT="Siva Parvathi Kalyanam" align="right">
+								<br><br><br>	
+								In 2011 for second anniversary we have performed Siva Parvathi Kalyanam and installed SriChakra Meru <br>
+								<br>
+								Now for the third anniversary between June 10 - 14 we are planning to install Sahasra Linga, Goddess Gayathri , Ucchista Ganapathi & Subrahmanya swami. Accurate details of the program will be published in upcoming events soon.
+								<br>
+								<center>
+								<IMG SRC="images/home/lingam.gif" WIDTH="120" HEIGHT="120" BORDER="0" ALT="Sahasra Lingam">
+								<img src="images/home/Gayathri.jpg" width="100" height="120" border="0" alt="Gayathri Devi">
+								<img src="images/home/ucchista1.jpg" width="100" height="120" border="0" alt="Ucchista Ganapathi">
+								<img src="images/home/subrahmanyu.jpg" width="100" height="120" border="0" alt="Subrahmanu"><br>
+																</center>
+								Sahasra Lingam has 1008 individual lingas depicted on one big Shiva linga.<br>
+								
+								Ucchista Ganapathi upasana is very significant for devi upasakas.
+
+								<br><br>
+
+								<b> <A NAME="SN"> When is Sharada Navarathri Pooja performed?</A> </b> <a href="#BK">Top</A><br>
+                                Navarathri Pooja is performed every year at the beginning of winter (Sept - Oct), the pooja is performed twice daily for nine days, each day one of the forms of Goddess is worshipped with Chatushasti upachara pooja (serving god with 64 upachara sevas) in morning & evening and respective homas are preformed, the navarathri pooja is concluded with Anna Daanam at the end.<br><br>
+
+								<b>What is the significance of performing Sharada Navarathri Pooja?</b><br>
+								Every day there are two time periods that are ideal for upasana (spiritual practice), first being Bramhi Kaalam (early morning between 4 - 5 Am, just before sunrise) and second is Pradosha Kaalam (evening 5 - 6 Pm, just before sunset). During these time periods the mind can be put under control easily, during other times of the day, we are under the control of our minds :-). The significance of performing Sharada Navaratri pooja is that during these nine days unlike the other days in the year, the mind can be easily put under control through out the day. By performing the Navarathri pooja, Goddess Sharada will help us control our mind and in turn control our Ego & Pride.<br>
+								<br><br>
+								<center>
+									Please visit the <a href="gallery.html">photogallery</a> for a pictorial history of the temple.<br>
+									All are welcome to join the prathista programs.
+								</center>
+								<br><br>
+								</div></td>
+								</tr>
+                        </table></td>
+                    </tr>
+                    <tr> 
+                      <td><img src="images/bottom.png" width="585" height="23"></td>
+                    </tr>
+                  </table></td>
+                <td align="center" valign="top">
+				
+<!-- Includes the side gallery -->
+<?php include("side_gallery.html"); ?>
+				  
+				  </td>
+              </tr>
+          </table></td>
+        </tr>
+        <tr> 
+          <td align="center">&nbsp;</td>
+        </tr>
+        <tr> 
+          <td align="center">
+	
+		  
+<!-- Includes the side gallery -->
+<?php include("footer.html"); ?>
+
+			
+			</td>
+        </tr>
+        <tr> 
+          <td>&nbsp;</td>
+        </tr>
+      </table></td>
+  </tr>
+</table>
+</BODY>
+</HTML>
